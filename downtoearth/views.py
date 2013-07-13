c9bf4c639e6756585f9d2fae1ada2be0ba3d55cf
Abhinandan <abhiin1947@gmail.com>
2013-07-13 18:07:23 +0530

    happada

--- downtoearth/views.py
+++ downtoearth/views.py
@@ -44,36 +44,36 @@ def login():
 
 @app.route('/api/add_comment', methods=['POST'])
 def add_comment():
-	comment = Comment(request.form['item_id'], request.form['comment'])
-	db.session.add(comment)
-	db.session.commit()
+    comment = Comment(request.form['item_id'], request.form['comment'])
+    db.session.add(comment)
+    db.session.commit()
 
 @app.route('/api/up_vote', methods=['POST'])
 def up_vote():
-	try:
-		vote = Vote(current_user.id, request.form['item_id'], request.form['comment_id'], True)
-		db.session.add(vote)
-		db.session.commit()
+    try:
+        vote = Vote(current_user.id, request.form['item_id'], request.form['comment_id'], True)
+        db.session.add(vote)
+        db.session.commit()
     except:
         print "Database error"
         return jsonify(data={"success":0, "error":"Database Error"})
-	return jsonify(data={"success":1, "message":"Comment Up Voted"})
+    return jsonify(data={"success":1, "message":"Comment Up Voted"})
 
 @app.route('/api/down_vote', methods=['POST'])
 def down_vote():
-	try:
-		vote = Vote(current_user.id, request.form['item_id'], request.form['comment_id'], False)
-		db.session.add(vote)
-		db.session.commit()
+    try:
+        vote = Vote(current_user.id, request.form['item_id'], request.form['comment_id'], False)
+        db.session.add(vote)
+        db.session.commit()
     except:
         print "Database error"
         return jsonify(data={"success":0, "error":"Database Error"})
-	return jsonify(data={"success":1, "message":"Comment Down Voted"})
+    return jsonify(data={"success":1, "message":"Comment Down Voted"})
 
 @app.route('/api/can_vote', methods=['POST'])
 def can_vote():
-	comment = Comment.query.filter_by(user_id = current_user.id).filter_by(comment_id = request.form['comment_id']).all()
-	if len(comment) > 0:
-		return jsonify(data={"can_vote":False})
-	else:
-		return jsonify(data={"can_vote":True})	
+    comment = Comment.query.filter_by(user_id = current_user.id).filter_by(comment_id = request.form['comment_id']).all()
+    if len(comment) > 0:
+        return jsonify(data={"can_vote":False})
+    else:
+        return jsonify(data={"can_vote":True})    
