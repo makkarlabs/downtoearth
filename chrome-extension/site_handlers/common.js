c0bf453774939c948532cdcd82faffa9893157fb
karthik <karthik.s.sundaram@gmail.com>
2013-07-13 18:53:46 +0530

    chrome extension skeleton added

---
+++ chrome-extension/site_handlers/common.js
@@ -0,0 +1,9 @@
+console.log("inside commons");
+function showDialogOnSite(dialog_text) {
+	console.log(dialog_text);
+        $.pnotify.defaults.history = false;
+	$.pnotify({
+		title: 'Regular Notice',
+		text: dialog_text
+	});
+}
---
+++ chrome-extension/site_handlers/dominos_co_in.js
@@ -0,0 +1,5 @@
+$(function() {
+    showDialogOnSite("write your html here");
+    console.log("You are hAcKeD! ha ha");
+});
+
