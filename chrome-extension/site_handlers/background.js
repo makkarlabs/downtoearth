2840a1f360cd2d55fb12ff010aea9f8a5bef7cab
realslimkarthik <karthikhariharan13@gmail.com>
2013-07-14 03:45:17 +0530

    chrome ext restaurantsor items are shown based on the current tab

---
+++ chrome-extension/site_handlers/background.js
@@ -0,0 +1,41 @@
+
+console.log("background started");
+injected_ids = [];
+injected_ids_map = {};
+chrome.runtime.onMessage.addListener(
+function(request, sender, sendResponse) {
+console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+if (request.site_url === "dominos_co_in") {
+    console.log("hello");
+    chrome.browserAction.setPopup({"popup":"site_handlers/dominos_co_in.html"});
+    injected_ids.push(sender.tab.id);
+    injected_ids_map[sender.tab.id] = "dominos_co_in";
+  sendResponse({farewell: "good"});
+}
+else {
+    chrome.browserAction.setPopup({"popup":"site_handlers/common.html"});
+  sendResponse({farewell: "bye"});
+}
+});
+
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log(activeInfo);    
+    if(injected_ids.indexOf(activeInfo.tabId) == -1) {
+        chrome.runtime.sendMessage({"site_url": "other"});
+    }
+    else {
+        chrome.browserAction.setPopup({"popup":"site_handlers/"+injected_ids_map[activeInfo.tabId]+".html"});
+    }
+});
+
+chrome.tabs.onRemoved.addListener(function( tabId,  removeInfo) {
+    if(injected_ids.indexOf(activeInfo.tabId) == -1) {
+        injected_ids.splice(injected_ids.indexOf(tabId), 1);
+    }
+    else {
+        injected_ids_map.remove(tabId);
+    }
+});
--- chrome-extension/site_handlers/dominos_co_in.js
+++ chrome-extension/site_handlers/dominos_co_in.js
@@ -1,6 +1,10 @@
 $(function() {
     showDialogOnSite("write your html here");
-    $("#GrapeVineFrame").src = "http://localhost:5000/restaurants/kfc";
-    $("#GrapeVineFrame").attr("src", function ( i, val ) { return val; });
+    chrome.runtime.sendMessage({"site_url": "dominos_co_in"}, function(response) {
+        console.log(response);
+    });
+    console.log($("#GrapeVineFrame").attr('src'));
+    //$("#GrapeVineFrame").src = "http://localhost:5000/restaurants/kfc";
+    //$("#GrapeVineFrame").attr("src", function ( i, val ) { return val; });
     console.log("You are hAcKeD! ha ha");
 });
